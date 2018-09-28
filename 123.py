d4cc1339481a2e4e86aa78a068738c819564389f
XStrafe <43400845+XStrafe@users.noreply.github.com>
2018-09-28 20:34:58 +1000

    Update 123.py

--- 123.py
+++ 123.py
@@ -154,4 +154,4 @@ async def udemy (ctx):
 
     print("{} Typed !udemy".format(userName))
     
-bot.run('NDkzODQ2NTY0MTgwMzI4NDc1.Doq89g.DGtitK6oCc2wFzVfDY38WXQQLao')
+bot.run('NDk1MTgxMDc4MTc3MzgyNDAw.Do-U7Q.6KnaR5jHVEwPO254mwFQD2wW_yU')
